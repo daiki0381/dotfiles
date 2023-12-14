d6a163c24ca0dba1e988f0e7f45107981ea5d6d5
daiki0381 <daiki03810218@gmail.com>
2023-12-14 18:40:16 +0900

    Update .config/nvim/after/plugin/bufferline.rc.lua

--- .config/nvim/after/plugin/bufferline.rc.lua
+++ .config/nvim/after/plugin/bufferline.rc.lua
@@ -4,7 +4,25 @@ if not status then
 end
 
 bufferline.setup({
-	options = { diagnostics = "nvim_lsp" },
+	options = {
+		diagnostics = "nvim_lsp",
+		offsets = {
+			{
+				filetype = "neo-tree",
+				text = "󰥨 File Explorer",
+				text_align = "left",
+				separator = true,
+			},
+		},
+	},
 })
 
+function close_current_buffer()
+	local current_buffer_id = vim.fn.bufnr("%")
+	vim.schedule(function()
+		vim.cmd("bd " .. current_buffer_id)
+	end)
+end
+
 vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<C-x>", close_current_buffer)
